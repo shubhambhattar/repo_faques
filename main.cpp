2b7fb96d38736909af5c4535c3d71b4196c6ea49
Shubham Bhattar <shubhambhattar@users.noreply.github.com>
2016-11-01 13:19:49 +0530

    `beg` and `end` should be 'mid+1` and `mid-1`. Check description please.
    `mid` should be `beg + (end - beg)/2` to avoid integer overflow if the size of array is large. Mainly a good programming practice.
    Added a few spaces and indentation to make code look good.

--- main.cpp
+++ main.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
     int n,key;
-    cout<<"enter no. of elements :-  ";
-    cin>>n;
+    cout << "Enter no. of elements :-  ";
+    cin >> n;
     int a[n];
-    cout<<"enter elements in increasing order\n";
-    for(int i=0;i<n;i++)
-        cin>>a[i];
-    cout<<"enter elements to be searched :- ";
-    cin>>key;
-    int beg=0,end=n,mid=(beg+end)/2;
-    while(beg<=end && a[mid]!=key){
-        if(a[mid]<key)
-            beg=mid;
+    cout << "Enter elements in increasing order\n";
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    cout << "Enter elements to be searched :- ";
+    cin >> key;
+    
+    int beg = 0, end = n-1, mid= beg + (end - beg)/2;
+    // While the size of the range to be searched is atleaset 1 and element not found:
+    while(beg <= end && a[mid] != key)
+    {
+        // If a[mid] < key, then the key will either be in range [mid+1, end], or not present. Similarly for end.        
+	    if(a[mid] < key)
+            beg = mid+1;
         else
-            end=mid;
-        mid = (beg+end)/2;
+            end = mid-1;
+        mid = beg + (end - beg)/2;
     }
-    if(a[mid]==key)
-    { cout<<key;
-        cout<<" found at position "<<mid+1;
-    }
+
+    if(a[mid] == key)
+	cout << key << " found at position " << mid+1 << endl;
     else
-        cout<<key<<" not found.";
+        cout << key << " not found." << endl;
     return 0;
 }
